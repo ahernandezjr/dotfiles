5504d30b6f1fe21613970d38f81f39ed35ee711c
Alexander Hernandez <ahernandezjr0@gmail.com>
2026-02-23 01:34:41 -0600

    fix: Update laptop specific configuration addition to fix lack of initrd kernel modules to prevent Blake2b hash mismatching

--- nixos/hosts/laptop/default.nix
+++ nixos/hosts/laptop/default.nix
@@ -5,4 +5,7 @@
       ./hardware-configuration.nix
       ../../common/configuration.nix
     ];
+
+  # Add essential kernel modules for laptop hardware to prevent kernel panic.
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
 }
